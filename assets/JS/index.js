90d05a6e95b8580e26488246b60e82f9ff27e010
Aurino Manoel <aurinodoc@gmail.com>
2023-03-02 17:54:47 -0300

    Commitando

---
+++ assets/JS/index.js
@@ -0,0 +1,38 @@
+function pegaOsSegundos (segundos){
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-br',{
+        hour12: false,
+        timeZone: 'UTC'
+    })
+}
+
+const relogio = document.querySelector('.clock');
+
+let segundos = 0;
+let timer;
+
+function startClock() {
+    timer = setInterval(function () {
+        segundos++;
+        relogio.innerHTML = pegaOsSegundos(segundos);
+    }, 1000);
+};
+
+document.addEventListener('click', function (event) {
+    const elemento = event.target;
+    if (elemento.classList.contains('start')){
+        relogio.classList.remove('pausado')
+        clearInterval(timer);
+        startClock();
+    }
+    if (elemento.classList.contains('pause')){
+        relogio.classList.add('pausado')
+        clearInterval(timer);
+    }
+    if (elemento.classList.contains('clear')){
+        clearInterval(timer);
+        relogio.classList.remove('pausado')
+        relogio.innerHTML = '00:00:00';
+        segundos = 0;
+    }
+});
